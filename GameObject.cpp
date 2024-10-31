5c36fb1f1e36a07633afc18999592df147a45869
linuschas <linus.nilsson@chasacademy.se>
2024-10-31 18:22:12 +0100

    feat(working file to compile & basic testing)

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,10 +1,13 @@
 #include "Player.h"
 #include "GameObject.h"
 #include <iostream>
+#include "utils/utils.cpp"
 
 GameObject::GameObject(/* args */)
 {
     // init the player/players?
+
+    std::cout << "Inside GameObject" << std::endl;
 }
 
 GameObject::~GameObject()
@@ -28,16 +31,15 @@ void GameObject::take_turn(Player)
     while(turn)
     {
         for( Player active_player : players ) {
-           std::pair <int, int> attackcoordinate;
-           std::string buf= "";
-           std::cin >> buf;
-           attackcoordinate=validate(buf);//from included validate
-           //check if coordinate is valid
-        // if valid, continue
-        // else further checking
+            std::string buf= "";
+            std::cin >> buf;
+
+            std::pair<int, int> attackcoordinate = validateCoordinate(buf);//from included validate
+            // Validate coordinates, -1, -1 is invalid
+            // Continue if valid, otherwise re-prompt
     for(Player player: players)
     {
-        player.mark(active_player);
+        // player.mark(active_player); // Temporarily disabled to get an executable, player is missing this method
     }
         }
 
---
+++ init.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "start_menu/start_menu.cpp"
+
+int main() {
+    std::cout << "Program running" << std::endl;
+
+    start_menu();
+
+    return 0;
+}
